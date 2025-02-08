d970be62191ec69980f72691914db5ecd946a8ed
VOLEHUYDAT <huydat13825@gmail.com>
2025-02-08 18:22:20 +0700

    8/2 -thanh toán

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 #Category
 class Category(models.Model):
     sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name= 'sub_categories', null=True, blank=True)
@@ -75,3 +76,18 @@ class ShippingAddress(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.address
+class PaymentForm(forms.Form):
+
+    order_id = forms.CharField(max_length=250)
+    order_type = forms.CharField(max_length=20)
+    amount = forms.IntegerField()
+    order_desc = forms.CharField(max_length=100)
+    bank_code = forms.CharField(max_length=20, required=False)
+    language = forms.CharField(max_length=2)
+
+class Payment_VNPay(models.Model):
+    order_id = models.BigIntegerField(default=0, null=True, blank=True)
+    amount = models.FloatField(default=0.0, null=True, blank=True)
+    order_desc = models.CharField(max_length=200,null=True,blank=True)
+    vnp_TransactionNo = models.CharField(max_length=200,null=True,blank=True)
+    vnp_ResponseCode = models.CharField(max_length=200, null=True, blank=True)
