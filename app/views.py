2692e9f284b822ddd93e28a9fd9c361960f60c98
NGUYENGIAHUNG <hungng2392@ut.edu.vn>
2025-02-11 23:18:11 +0700

    them review

--- app/views.py
+++ app/views.py
@@ -52,8 +52,9 @@ def detail(request):
         cartItems = order['get_cart_items']
     id = request.GET.get('id','')
     products = Product.objects.filter(id=id)
+    reviews = ReviewRating.objects.filter(product_id__in=products.values_list('id', flat=True), status=True)
     categories = Category.objects.filter(is_sub =False)
-    context={'products':products, 'items':items,'order':order,'cartItems':cartItems,'categories':categories}
+    context={'products':products, 'items':items,'order':order,'cartItems':cartItems,'categories':categories,'reviews': reviews}
     return render(request, 'app/detail.html', context)
 def category(request):
     categories = Category.objects.filter(is_sub =False)
@@ -546,3 +547,26 @@ def contact(request):
         return redirect('home')  
 
     return render(request, 'app/contact.html')
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Your review has been updated.')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(url)
